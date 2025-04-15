f2417fa9e37141845c0acc31a08079c23256ce81
jooho172200 <jooho172200@gmail.com>
2025-04-15 15:45:57 +0900

    initial commit

---
+++ src/main/java/com/goldstone/saboteur_backend/SaboteurApplication.java
@@ -0,0 +1,13 @@
+package com.goldstone.saboteur_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaboteurApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaboteurApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/goldstone/saboteur_backend/SaboteurApplicationTests.java
@@ -0,0 +1,13 @@
+package com.goldstone.saboteur_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaboteurApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
