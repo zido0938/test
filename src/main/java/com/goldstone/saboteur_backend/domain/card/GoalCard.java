65c686e9497f9f8430a928eba939696f98c7e050
jooho172200 <jooho172200@gmail.com>
2025-05-20 00:31:05 +0900

    도메인 수정 -GoalCard 수정

--- src/main/java/com/goldstone/saboteur_backend/domain/card/GoalCard.java
+++ src/main/java/com/goldstone/saboteur_backend/domain/card/GoalCard.java
@@ -8,12 +8,9 @@ import lombok.NoArgsConstructor;
 
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
 public class GoalCard extends PathCard {
     private GoalCardType type;
-
-    public GoalCard() {
-        super(PathCardType.CROSSROAD, false);
-    }
 
     @Override
     public void use() {}
