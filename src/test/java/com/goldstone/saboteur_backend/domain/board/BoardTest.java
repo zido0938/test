9c991c1964c946377e1bae1cf3cf28155dcc52e7
박지호 <jiho5993@gmail.com>
2025-05-21 16:38:24 +0000

    test: Board 도메인 리팩토링에 따른 테스트코드 수정 및 작성

--- src/test/java/com/goldstone/saboteur_backend/domain/board/BoardTest.java
+++ src/test/java/com/goldstone/saboteur_backend/domain/board/BoardTest.java
@@ -2,11 +2,19 @@ package com.goldstone.saboteur_backend.domain.board;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import com.goldstone.saboteur_backend.domain.card.GoalCard;
 import com.goldstone.saboteur_backend.domain.card.PathCard;
+import com.goldstone.saboteur_backend.domain.card.StartCard;
 import com.goldstone.saboteur_backend.domain.enums.PathCardType;
+import com.goldstone.saboteur_backend.domain.enums.PathType;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Set;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+@DisplayName("Board Test")
 class BoardTest {
 
     @Test
@@ -15,31 +23,109 @@ class BoardTest {
         Board board = new Board();
 
         assertEquals(45, board.getDynamicCells().size());
-        assertEquals(45, board.getInitArea().size());
+
+        Set<Cell> dynamicCells = board.getDynamicCells();
+        Cell targetCell;
+        Cell findedCell;
 
         // start card
-        assertFalse(board.getCell(new Position(0, 2)).isEmptyCard());
+        targetCell = new Cell(0, Board.DEFAULT_HEIGHT / 2);
+        findedCell = dynamicCells.stream().filter(targetCell::equals).findFirst().get();
+        assertNotNull(findedCell);
+        assertFalse(findedCell.isEmptyCard());
 
         // goal card
-        assertFalse(board.getCell(new Position(8, 0)).isEmptyCard());
-        assertFalse(board.getCell(new Position(8, 2)).isEmptyCard());
-        assertFalse(board.getCell(new Position(8, 4)).isEmptyCard());
+        targetCell = new Cell(Board.DEFAULT_WIDTH - 1, 0);
+        findedCell = dynamicCells.stream().filter(targetCell::equals).findFirst().get();
+        assertNotNull(findedCell);
+        assertFalse(findedCell.isEmptyCard());
+
+        targetCell = new Cell(Board.DEFAULT_WIDTH - 1, 2);
+        findedCell = dynamicCells.stream().filter(targetCell::equals).findFirst().get();
+        assertNotNull(findedCell);
+        assertFalse(findedCell.isEmptyCard());
+
+        targetCell = new Cell(Board.DEFAULT_WIDTH - 1, 4);
+        findedCell = dynamicCells.stream().filter(targetCell::equals).findFirst().get();
+        assertNotNull(findedCell);
+        assertFalse(findedCell.isEmptyCard());
     }
 
     @Test
-    void isValidPosition() {}
+    @DisplayName("x, y 좌표로부터 Cell에 대한 정보를 얻을 수 있다.")
+    void getCellFromXAndY() {
+        Board board = new Board();
+        Cell startCell = board.getCellFromXAndY(0, 2);
+
+        assertNotNull(startCell);
+        assertEquals(0, startCell.getX());
+        assertEquals(2, startCell.getY());
+        assertFalse(startCell.isEmptyCard());
+        assertInstanceOf(StartCard.class, startCell.getCard());
+
+        PathType[] sides = startCell.getSides();
+        for (int i = 0; i < 4; i++) {
+            assertEquals(PathType.PATH, sides[i]);
+        }
+    }
 
     @Test
-    void placeCard() {}
+    @DisplayName("원하는 좌표에 카드를 놓을 수 있다.")
+    void placeCard() {
+        Board board = new Board();
+        int x = 1;
+        int y = 3;
+
+        boolean result = board.placeCard(x, y, new PathCard(PathCardType.CROSSROAD, false));
+        assertTrue(result);
+
+        Cell newCell = board.getCellFromXAndY(x, y);
+
+        assertNotNull(newCell);
+        assertEquals(x, newCell.getX());
+        assertEquals(y, newCell.getY());
+        assertFalse(newCell.isEmptyCard());
+        assertInstanceOf(PathCard.class, newCell.getCard());
+
+        PathType[] sides = newCell.getSides();
+        for (int i = 0; i < 4; i++) {
+            assertEquals(PathType.PATH, sides[i]);
+        }
+    }
 
     @Test
-    void getCell() {}
+    @DisplayName("이미 존재하는 Cell을 조회할 수 있다. 없는 Cell을 조회하려는 경우, 보드에 새로운 Cell이 추가되고, 보드의 크기가 커진다.")
+    void getOrCreateCell() {
+        Board board = new Board();
+
+        Cell startCell = board.getOrCreateCell(0, 2);
+        assertNotNull(startCell);
+        assertEquals(45, board.getDynamicCells().size());
+
+        // 시작 셀 왼족
+        int x = -1;
+        int y = 2;
+        Cell newCell = board.getOrCreateCell(x, y);
+        assertNotNull(newCell);
+        assertEquals(46, board.getDynamicCells().size());
+    }
 
     @Test
-    void getOrCreateCell() {}
+    @DisplayName("GoalCell 리스트와 각 GoalCell에 대한 정보를 얻을 수 있다.")
+    void getGoals() {
+        Board board = new Board();
 
-    @Test
-    void getGoals() {}
+        Cell goalCells[] = board.getGoals();
+
+        assertNotNull(goalCells);
+        assertEquals(3, goalCells.length);
+
+        for (int i = 0; i < goalCells.length; i++) {
+            assertInstanceOf(GoalCard.class, goalCells[i].getCard());
+            assertEquals(Board.DEFAULT_WIDTH - 1, goalCells[i].getX());
+            assertTrue(Arrays.asList(Board.DEFAULT_GOAL_CELL_Y_LIST).contains(goalCells[i].getY()));
+        }
+    }
 
     @Test
     @DisplayName("startCell부터 가로로 이어지는 pathCard를 셋팅했을 때, 모든 길은 이어져있다.")
@@ -47,25 +133,22 @@ class BoardTest {
         Board board = new Board();
 
         PathCard pathCard = new PathCard(PathCardType.CROSSROAD, false);
-        Position positions[] = {
-            new Position(1, 0),
-            new Position(2, 0),
-            new Position(3, 0),
-            new Position(4, 0),
-            new Position(5, 0),
-            new Position(6, 0),
-            new Position(7, 0),
+        int pos[][] = {
+            {1, 0}, {2, 0}, {3, 0}, {4, 0}, {5, 0}, {6, 0}, {7, 0},
         };
 
-        for (int i = 0; i < positions.length; i++) {
-            board.placeCard(positions[i], pathCard);
+        for (int i = 0; i < pos.length; i++) {
+            int x = pos[i][0], y = pos[i][1];
+            board.placeCard(x, y, pathCard);
         }
 
-        for (int i = 0; i < positions.length; i++) {
-            int x = positions[i].getX();
-            int y = positions[i].getY();
+        for (int i = 0; i < pos.length; i++) {
+            int x = pos[i][0], y = pos[i][1];
 
-            assertTrue(board.isConnected(new Position(x - 1, y), positions[i]));
+            Cell fromCell = board.getCellFromXAndY(x - 1, y);
+            Cell toCell = board.getCellFromXAndY(x, y);
+
+            assertTrue(board.isConnected(fromCell, toCell));
         }
     }
 
@@ -75,32 +158,28 @@ class BoardTest {
         Board board = new Board();
 
         assertEquals(45, board.getDynamicCells().size());
-        assertEquals(45, board.getInitArea().size());
 
         PathCard pathCard = new PathCard(PathCardType.CROSSROAD, false);
-        Position positions[] = {
-            new Position(0, 3),
-            new Position(0, 4), // 여기까지가 DEFAULT_HEIGHT에 의해 생성된 높이
-            new Position(0, 5),
-            new Position(0, 6),
-            new Position(0, 7),
-            new Position(0, 8),
-            new Position(0, 9),
-            new Position(0, 10),
+        int pos[][] = {
+            {0, 3}, {0, 4}, // 여기까지가 DEFAULT_HEIGHT에 의해 생성된 높이
+            {0, 5}, {0, 6}, {0, 7}, {0, 8}, {0, 9}, {0, 10},
         };
 
-        for (int i = 0; i < positions.length; i++) {
-            board.placeCard(positions[i], pathCard);
+        for (int i = 0; i < pos.length; i++) {
+            int x = pos[i][0], y = pos[i][1];
+            board.placeCard(x, y, pathCard);
         }
 
         assertNotEquals(45, board.getDynamicCells().size());
         assertEquals(45 + 6, board.getDynamicCells().size()); // 6개의 Cell이 추가됨
 
-        for (int i = 0; i < positions.length; i++) {
-            int x = positions[i].getX();
-            int y = positions[i].getY();
+        for (int i = 0; i < pos.length; i++) {
+            int x = pos[i][0], y = pos[i][1];
 
-            assertTrue(board.isConnected(new Position(x, y - 1), positions[i]));
+            Cell fromCell = board.getCellFromXAndY(x, y - 1);
+            Cell toCell = board.getCellFromXAndY(x, y);
+
+            assertTrue(board.isConnected(fromCell, toCell));
         }
     }
 }
